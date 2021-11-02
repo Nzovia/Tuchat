1993f5092520ed226620e21da3ae6001b198ffc8
Nicholas Maundu <nickiemaundu19@gmail.com>
2021-11-02 13:09:25 +0300

    Created a settings screen using preference library that constitutes preference Containers i.e PreferenceScreen and preferenceCategory then individual Preference types that include preference, edittextPreference, listPreference, SwitchPreferenceCompat and CheckBox

--- app/src/main/java/com/nicholas/tuchat/SettingsFragment.kt
+++ app/src/main/java/com/nicholas/tuchat/SettingsFragment.kt
@@ -6,23 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.PreferenceFragmentCompat
 
 
-class SettingsFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_settings, container, false)
-
-        view.setOnClickListener {
-            val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
-            val navController = navHostFragment.navController
-            val action = SettingsFragmentDirections.actionSettingsToAccSettings()
-            navController.navigate(action)
-        }
-
-        return view
+class SettingsFragment : PreferenceFragmentCompat() {
+    //this class supports the fragment UI
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
     }
+    
 }
